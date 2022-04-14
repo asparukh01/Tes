9e60e35684582f9f8b8c26ea8c6ee8b13c68be18
Мейрамбеков аспарух <asparukh2001@gmail.com>
2022-04-15 00:32:28 +0600

    Добавил модель резюме

--- headhunter_app/models.py
+++ headhunter_app/models.py
@@ -49,17 +49,35 @@ class TodoTask(IsDeletedMixin):
     def __str__(self):
         return "№{} {}".format(self.pk, self.title) 
 
+
+class Resume(IsDeletedMixin):
+    title = models.CharField(max_length=50, null=False, blank=False, verbose_name='Название задачи',
+                          validators=[MinLengthValidator(3)])
+    description = models.TextField(max_length=500, null=True, blank=True, verbose_name='Описание задачи', validators=[WordLengthValidator(50)])
+    infos = models.ManyToManyField(to='headhunter_app.Info', related_name='infos', verbose_name='Дополнительная информация', blank=True)
+    telegram = models.CharField(max_length=50, null=True, blank=True, verbose_name='Телеграм',
+                             validators=[MinLengthValidator(3)])
+    email =models.EmailField(max_length=50, null=False, blank=False, verbose_name='Email')
+    phone_number =models.IntegerField(max_length=12, null=False, blank=False, verbose_name='Телефон')
+    facebook = models.CharField(max_length=50, null=True, blank=True, verbose_name='Facebook',
+                             validators=[MinLengthValidator(3)])
+    linkedin = models.CharField(max_length=50, null=True, blank=True, verbose_name='Linkedin',
+                             validators=[MinLengthValidator(3)])
+    # status = models.ForeignKey(to='headhunter_app.TaskStatus', on_delete=models.RESTRICT, related_name='statuses', verbose_name='Статус')
+    # project =  models.ForeignKey(
+    #     to='todoapp.TodoProject',
+    #     on_delete=models.CASCADE,
+    #     related_name='project',
+    #     verbose_name='Проект',
+    #     default=1)
+    # created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    # updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+
+
 class Info(IsDeletedMixin):
     title = models.CharField(max_length=25, null=False, blank=False, verbose_name='Название поля', validators=[MinLengthValidator(3)])
     description = models.TextField(max_length=500, null=True, blank=True, verbose_name='Описание поля', validators=[WordLengthValidator(50)])
     
-    def __str__(self):
-        return f"{self.title}"
-
-
-class Category(IsDeletedMixin):
-    title = models.CharField(max_length=20, null=False, blank=False, verbose_name='Категория')
-
     def __str__(self):
         return f"{self.title}"
 
